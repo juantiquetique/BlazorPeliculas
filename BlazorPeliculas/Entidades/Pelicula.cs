2550e10f93cddf0b0700a15523de1093265fb186
juantiquetique <juandavidtiquetique172@gmail.com>
2026-03-04 22:30:49 -0500

    Add project files.

---
+++ BlazorPeliculas/Entidades/Pelicula.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorPeliculas.Entidades;
+
+public class Pelicula
+{
+    public int Id { get; set; }
+    public required string Titulo { get; set; }
+    public DateTime FechaLanzamiento { get; set; }
+}
