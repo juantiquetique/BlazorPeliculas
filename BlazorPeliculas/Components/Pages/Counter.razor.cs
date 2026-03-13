bf32989319e19d044d1981b2d310cbea049a8751
JuanTT <juandavidtiquetique172@gmail.com>
2026-03-13 16:20:58 -0500

    13/03/2026 JT

---
+++ BlazorPeliculas/Components/Pages/Counter.razor.cs
@@ -0,0 +1,16 @@
+﻿using BlazorPeliculas.Entidades;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BlazorPeliculas.Components.Pages;
+
+public partial class Counter (ServicioSingleton singleton, ServicioTransient transient, ServicioScoped scoped2)
+{
+    private int currentCount = 0;
+    private void IncrementCount()
+    {
+        currentCount++;
+        transient.valor = currentCount;
+        singleton.valor = currentCount;
+        scoped2.valor = currentCount;
+    }
+}
