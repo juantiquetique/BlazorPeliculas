c1020d2b39a1365d8cb682d057feddbcbcc7e337
JuanTT <juandavidtiquetique172@gmail.com>
2026-03-27 16:32:39 -0500

    27/03/2026

--- BlazorPeliculas/Entidades/Genero.cs
+++ BlazorPeliculas/Entidades/Genero.cs
@@ -1,7 +1,12 @@
-﻿namespace BlazorPeliculas.Entidades;
+﻿using BlazorPeliculas.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorPeliculas.Entidades;
 
 public class Genero
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "El campo {0} es requerido.")]
+    [PrimeraLetraMayuscula]
     public string? Nombre { get; set; }
 }
